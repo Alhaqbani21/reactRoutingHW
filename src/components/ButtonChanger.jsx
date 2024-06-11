7848d4e63839265752a965ae86628266710b27b7
Abdulaziz Alhaqbani <alhaqbani21@gmail.com>
2024-06-12 02:02:20 +0300

    update

--- src/components/ButtonChanger.jsx
+++ src/components/ButtonChanger.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function ButtonChanger(props) {
   return (
     <div class="block max-w-[18rem] rounded-lg bg-gray-500 text-left text-slate-900 hover:scale-110 hover:cursor-pointer hover:text-white max-md:w-[7rem]">
-      <div class="max-md:p-4 text-center">
+      <div class="max-md:p-4 text-center p-12">
         <h5 class="mb-1 text-xl font-medium leading-tight text-center max-md:text-lg">
           {props.title}
         </h5>
